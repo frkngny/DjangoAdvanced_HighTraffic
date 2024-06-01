cf18d22b13e8905c928898850433e873e4c1da23
frkngny <frkang35@gmail.com>
2024-06-02 01:41:14 +0300

    generic relation to profile page

--- profilepage/models.py
+++ profilepage/models.py
@@ -5,11 +5,12 @@ from useraccount.models import UserAccount
 from business.models import Business
 
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 
 class ProfilePage(models.Model):
-    user_account = models.ForeignKey(UserAccount, null=True, blank=False, on_delete=CASCADE)
-
-    def __str__(self):
-        return '{}'.format(self.user_account.first_name)
+    # user_account = models.ForeignKey(UserAccount, null=True, blank=False, on_delete=CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=CASCADE, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    subject = GenericForeignKey('content_type', 'object_id')
--- profilepage/views.py
+++ profilepage/views.py
@@ -11,10 +11,14 @@ import datetime
 
 def profile_detail(request, profilepage_id):
     template = loader.get_template('profilepage.html')
-    profile = ProfilePage.objects.get(user_account=UserAccount.objects.get(user=request.user).id)
-    Post = apps.get_model('post', 'Post')
-    posts = Post.objects.filter(author=profile.user_account.user)
-    own_profile = request.user and (request.user.id == profile.user_account.user.id)
+    profile = ProfilePage.objects.get(object_id=profilepage_id)
+    print(profile.subject)
+    posts = []
+    own_profile = False
+    if profile.content_type.model == 'useraccount':
+        Post = apps.get_model('post', 'Post')
+        posts = Post.objects.filter(author=profile.subject.user)
+        own_profile = request.user and (request.user.id == profile.subject.user.id)
     return HttpResponse(template.render({'profile': profile, 'posts': posts, 'own_profile': own_profile}, request))
 
 
